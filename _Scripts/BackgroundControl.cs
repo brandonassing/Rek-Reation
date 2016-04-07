5c35015e0aa2b04c3f429c7e9ee66c6bd053c484
brandonassing <bassing@uwo.ca>
2016-04-06 20:17:57 -0400

    Fixed scaling on main menu

--- _Scripts/BackgroundControl.cs
+++ _Scripts/BackgroundControl.cs
@@ -17,27 +17,27 @@ public class BackgroundControl : MonoBehaviour
     void Start()
     {
         //Makes user input consistent between scene changes
-        background.GetComponent<Renderer>().material = listMats[UserValidation.userList[UserValidation.activeUserIndex].backgroundChoice];
+        background.GetComponent<Image>().material = listMats[UserValidation.userList[UserValidation.activeUserIndex].backgroundChoice];
 
         //Prevents null reference exception
             if(SceneManager.GetActiveScene().name == "Main Player Menu")
         {
             dropDownBack.value = UserValidation.userList[UserValidation.activeUserIndex].backgroundChoice;
-            displayBack.GetComponent<Renderer>().material = listMats[dropDownBack.value];
+            displayBack.GetComponent<Image>().material = listMats[dropDownBack.value];
         }
     }
 
     //Sets the background display preview box
     public void SetBackgroundChoice()
     {
-        displayBack.GetComponent<Renderer>().material = listMats[dropDownBack.value];
+        displayBack.GetComponent<Image>().material = listMats[dropDownBack.value];
     }
 
     //Sets the game background
     public void SetBackground()
     {
         UserValidation.userList[UserValidation.activeUserIndex].backgroundChoice = dropDownBack.value;
-        background.GetComponent<Renderer>().material = listMats[UserValidation.userList[UserValidation.activeUserIndex].backgroundChoice];
+        background.GetComponent<Image>().material = listMats[UserValidation.userList[UserValidation.activeUserIndex].backgroundChoice];
         UserValidation.Save();
     }
 
